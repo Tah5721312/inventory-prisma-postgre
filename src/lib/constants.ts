06618eb9d19a68914f05294c8fa04fc6db96f207
tah5721312 <tah5721312@gmail.com>
2025-11-22 23:54:01 +0200

    tah4

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,8 @@
 export const ARTICLE_PER_PAGE = 6;
 
-// const PRODUCTION_DOMAIN = "https://nextjs-tah.vercel.app";
+const PRODUCTION_DOMAIN = "https://inventory-prisma-postgre.vercel.app";
 // const PRODUCTION_DOMAIN = "http://192.168.58.63:3000";
-const PRODUCTION_DOMAIN = "http://localhost:3000";
+//const PRODUCTION_DOMAIN = "http://localhost:3000";
 
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 // const DEVELOPMENT_DOMAIN = "http://192.168.58.63:3000"; // ← غيّر لـ IP جهاز السيرفر
